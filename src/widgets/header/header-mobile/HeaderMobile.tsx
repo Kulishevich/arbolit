f8c8c9a22a25620a0ecbf1e786419feab1724126
Maksim Kulishevich <maxim8992012@gmail.com>
2025-04-11 11:51:57 +0300

    fix

--- src/widgets/header/header-mobile/HeaderMobile.tsx
+++ src/widgets/header/header-mobile/HeaderMobile.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './HeaderMobile.module.scss';
 import { Logo } from '@/entities/logo';
-import { BurgerMenuIcon, PhoneIcon } from '@/shared/assets/icons';
+import { PhoneIcon } from '@/shared/assets/icons';
 import { Button } from '@/shared/ui/button';
 import { FeedbackPopup } from '@/entities/feedback-popup/FeedbackPopup';
 import { HeaderBurgerMenu } from '@/features/header-burger-menu';
