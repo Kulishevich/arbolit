2f7b05aeb3e9297c2430856cbd4de4fd4c2ab305
Maksim Kulishevich <maxim8992012@gmail.com>
2025-04-17 16:52:41 +0300

    feat: add shadow in header descktop

--- src/widgets/header/header-desktop/HeaderDesktop.tsx
+++ src/widgets/header/header-desktop/HeaderDesktop.tsx
@@ -35,56 +35,58 @@ export const HeaderDesktop = ({
     })) || [];
 
   return (
-    <div className={s.container}>
-      <div className={s.info}>
-        <Logo logo={setting?.logo_path} />
-        <div className={s.address}>
-          <LocationIcon />
-          <p className="body-4">{setting?.address}</p>
-        </div>
-        <div className={s.phones}>
-          <PhoneIcon />
-          <div>
-            {setting?.phones.map((phone, index) => {
-              const cleanedPhone = phone.replace(/[^+\d]/g, '');
-              return (
-                <Link
-                  href={`tel:${cleanedPhone}`}
-                  className="body-3"
-                  key={index}
-                >
-                  {phone}
-                </Link>
-              );
-            })}
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <div className={s.info}>
+          <Logo logo={setting?.logo_path} />
+          <div className={s.address}>
+            <LocationIcon />
+            <p className="body-4">{setting?.address}</p>
           </div>
+          <div className={s.phones}>
+            <PhoneIcon />
+            <div>
+              {setting?.phones.map((phone, index) => {
+                const cleanedPhone = phone.replace(/[^+\d]/g, '');
+                return (
+                  <Link
+                    href={`tel:${cleanedPhone}`}
+                    className="body-3"
+                    key={index}
+                  >
+                    {phone}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+          <FeedbackPopup>
+            <Button variant="callback">Обратный звонок</Button>
+          </FeedbackPopup>
         </div>
-        <FeedbackPopup>
-          <Button variant="callback">Обратный звонок</Button>
-        </FeedbackPopup>
-      </div>
-      <div className={s.line}></div>
-      <div className={s.navigation}>
-        <Link className="t-header" href={navigation[0].path}>
-          {navigation[0].title}
-        </Link>
-        <HeaderDropdown
-          title="Каталог"
-          href={paths.catalog}
-          items={catalogProducts}
-          className="t-header"
-        />
-        <HeaderDropdown
-          href={paths.about}
-          title="О компании"
-          items={aboutCompany}
-          className="t-header"
-        />
-        {navigation.slice(1).map((nav, index) => (
-          <Link className="t-header" key={index} href={nav.path}>
-            {nav.title}
+        <div className={s.line}></div>
+        <div className={s.navigation}>
+          <Link className="t-header" href={navigation[0].path}>
+            {navigation[0].title}
           </Link>
-        ))}
+          <HeaderDropdown
+            title="Каталог"
+            href={paths.catalog}
+            items={catalogProducts}
+            className="t-header"
+          />
+          <HeaderDropdown
+            href={paths.about}
+            title="О компании"
+            items={aboutCompany}
+            className="t-header"
+          />
+          {navigation.slice(1).map((nav, index) => (
+            <Link className="t-header" key={index} href={nav.path}>
+              {nav.title}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
