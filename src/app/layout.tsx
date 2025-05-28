27e083c347960aad6b67e99707e8690bfaeb9667
Maksim Kulishevich <maxim8992012@gmail.com>
2025-05-28 20:05:08 +0300

    feat: add google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,8 +49,31 @@ export default async function RootLayout({
   const blockStatus = await getBlockStatus();
 
   return (
-    <html lang="en">
+    <html lang="ru">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PDW237MF');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${manropeSans.variable} ${notoSans.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PDW237MF"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager */}
         <Script
           src={`https://api-maps.yandex.ru/v3/?apikey=${process.env.NEXT_PUBLIC_YANDEX_API_KEY}&lang=ru_RU`}
           strategy="beforeInteractive"
