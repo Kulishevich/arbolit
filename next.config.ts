de4fc308589b3dd65212c2ab80a0bba16f7a870b
Maksim Kulishevich <maxim8992012@gmail.com>
2025-07-10 18:55:37 +0300

    feat: add config redirects

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,46 @@ const nextConfig: NextConfig = {
       { source: '/feed.xml', destination: '/api/feed' },
     ];
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.(?<domain>.*)',
+          },
+        ],
+        destination: 'https://:domain/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+        ],
+        destination: 'https://:host/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-ssl',
+            value: 'off',
+          },
+        ],
+        destination: 'https://:host/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
