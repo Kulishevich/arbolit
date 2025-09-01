552994b4fa99c93eb8d6e4bd684cbb420cdb0f7f
Maksim Kulishevich <maxim8992012@gmail.com>
2025-09-01 12:26:17 +0300

    fix: delete google manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,17 +78,6 @@ export default async function RootLayout({
           `}
         </Script>
 
-        {/* Google Tag Manager */}
-        <Script id="gtm-init" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PDW237MF');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
         <meta name="yandex-verification" content="7bff8869bacf9f20" />
         <meta
           name="google-site-verification"
@@ -96,16 +85,6 @@ export default async function RootLayout({
         />
       </head>
       <body className={`${manropeSans.variable} ${notoSans.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PDW237MF"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager */}
         <Script
           src={`https://api-maps.yandex.ru/v3/?apikey=${process.env.NEXT_PUBLIC_YANDEX_API_KEY}&lang=ru_RU`}
           strategy="beforeInteractive"
